c8d076ea54c00c547592707aba1946fef4b964d7
udaysk3 <burluudaysantoshkumar3@gmail.com>
2024-01-18 22:42:39 +0530

    INIT

--- reform_crm/settings.py
+++ reform_crm/settings.py
@@ -123,13 +123,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
 import os
 STATIC_URL = '/static/'
-STATICFILES_DIRS  = [
-    os.path.join(BASE_DIR, 'static')
-]
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
