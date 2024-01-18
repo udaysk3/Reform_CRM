915fdc851c45dd7557fa6a08fa91b76f9f75d4bc
udaysk3 <burluudaysantoshkumar3@gmail.com>
2024-01-18 22:58:21 +0530

    INIT

--- reform_crm/settings.py
+++ reform_crm/settings.py
@@ -126,8 +126,9 @@ USE_TZ = True
 
 import os
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static') ]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+print(os.path.join(BASE_DIR, 'static'))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
