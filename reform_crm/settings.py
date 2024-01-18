a6275b28286c1cbc11be8dad1ada9f85018c4c62
udaysk3 <burluudaysantoshkumar3@gmail.com>
2024-01-18 23:37:40 +0530

    INIT

--- reform_crm/settings.py
+++ reform_crm/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--1*1d%q70kmpf=jhm8(v9k^=ly^42t*encf%)3kj_tfxi35tx('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
