531b9e07a77597bc1134241ae83e72142fbab2b0
udaysk3 <burluudaysantoshkumar3@gmail.com>
2024-01-18 23:11:08 +0530

    INIT

--- reform_crm/settings.py
+++ reform_crm/settings.py
@@ -130,7 +130,7 @@ import os
 # print(os.path.join(BASE_DIR, 'static'))
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static/') ]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
