6c10fdb7f41463044a159cc42e3e48f807487756
p-sai-gowtham <puvvulasaigowtham@gmail.com>
2024-05-15 18:34:48 +0530

    update mail

--- home/views.py
+++ home/views.py
@@ -1647,7 +1647,7 @@ def get_message(historyId):
                     )
             
             
-            history = HistoryId.objects.create(history_id=historyId, created_at=datetime.now(pytz.timezone("Europe/London")))
+            history = HistoryId.objects.create(history_id=response["historyId"], created_at=datetime.now(pytz.timezone("Europe/London")))
             
         except HttpError as error:
             # TODO(developer) - Handle errors from gmail API.
