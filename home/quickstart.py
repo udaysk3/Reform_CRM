108670468a813af400dbdcacc84f75a9b09e4ad1
p-sai-gowtham <puvvulasaigowtham@gmail.com>
2024-06-07 09:17:06 +0530

    update

--- home/quickstart.py
+++ home/quickstart.py
@@ -6,21 +6,13 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
 
 def main():
-    """Shows basic usage of the Gmail API.
-  Lists the user's Gmail labels.
-  """
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists("../static/token.json"):
         creds = Credentials.from_authorized_user_file("../static/token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -35,7 +27,7 @@ def main():
             token.write(creds.to_json())
 
     try:
-        TOPIC_NAME = "projects/nimble-factor-423106-t0/topics/ReformTopic"
+        TOPIC_NAME = "projects/pubsubproject-425703/topics/Reform"
 
         # Create the Gmail API client
         gmail = googleapiclient.discovery.build('gmail', 'v1', credentials=creds)
