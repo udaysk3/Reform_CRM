509407dbed81f1e22113b909ffb21b965bcfcca2
Venkata Siddardha Rali <sidhurv8@gmail.com>
2024-11-05 22:24:06 +0530

    added new migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ set -e
 # Run database migrations
 python manage.py migrate
 python manage.py create_superusers
-
+python manage.py create_cities
+python manage.py create_countys_and_countrires
+python manage.py create_uk
 # Start Gunicorn
 exec "$@"
